b1a10541427cadbd1bc074abb96b2971dd0c41ef
Vasco-jofra <joaovascofranco@hotmail.com>
2017-04-15 13:53:21 +0000

    GET parameters missing detection

--- web-server/WebServer.java
+++ web-server/WebServer.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
@@ -9,11 +11,20 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 // java -Djava.awt.headless=true -cp src raytracer.Main test05.txt test05.bmp 400 300 400 300 400 300
+// http://ec2-35-177-250-90.eu-west-2.compute.amazonaws.com:8000/r.html?f=aaa&sc=400&sr=300&wc=400&wr=300&coff=400&roff=300
 public class WebServer {
     public static final String CONTEXT = "/r.html";
     public static final int PORT = 8000;
-    
+    public static final List<String> required_params = new ArrayList<String>();
+
     public static void main(String[] args) throws Exception {
+        required_params.add("f");
+        required_params.add("sc");
+        required_params.add("sr");
+        required_params.add("wc");
+        required_params.add("sr");
+        required_params.add("coff");
+        required_params.add("roff");
         HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
         server.createContext(CONTEXT, new MyHandler());
         server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool());
@@ -32,7 +43,25 @@ public class WebServer {
 	            response += " - <b>" + entry.getKey() + "</b>: " + entry.getValue() + "<br>";
                 }
             }
-            t.sendResponseHeaders(200, response.length());
+            
+            boolean has_all_params = true;
+            int response_code = 200;
+            for(String p: required_params) {
+               if(params.containsKey(p) == false) {
+                   has_all_params = false;
+                   response_code = 400;
+                   System.out.println(p);
+                   break;
+               }
+            }
+            
+            if(has_all_params == true) {
+                response += "SUCCESS";
+            } else {
+                response += "MISSING_PARAMS";
+            } 
+
+            t.sendResponseHeaders(response_code, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
@@ -40,10 +69,10 @@ public class WebServer {
     }
     
     public static Map<String, String> queryToMap(String query){
-        if(query == null) {
-            return null;
-        }
         Map<String, String> result = new HashMap<String, String>();
+        if(query == null) {
+            return result;
+        }
         for (String param : query.split("&")) {
             String pair[] = param.split("=");
             if (pair.length>1) {
